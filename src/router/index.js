46610b5ebedd3deaeb2f9a52029a26c64a816a1d
canlandazzle <1481086138@qq.com>
2021-02-19 20:45:49 +0800

    新增路由页

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,26 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: () => import('@/components/Login')
+    },
+    {
+      path: '/notebooks',
+      name: 'NotebookList',
+      component: () => import('@/components/NotebookList')
+    },
+    {
+      path: '/note/:noteId',
+      name: 'NoteDetail',
+      component: () => import('@/components/NoteDetail')
+    },
+    {
+      path: '/trash/:noteId',
+      name: 'TrashDetail',
+      component: () => import('@/components/TrashDetail')
     }
   ]
 })
