c1da36b0d4081473f62a8383fe93b6480eba587e
canlandazzle <1481086138@qq.com>
2021-03-14 20:23:40 +0800

    element-ui message提示成功

--- src/store/modules/notebook.js
+++ src/store/modules/notebook.js
@@ -1,4 +1,6 @@
 import Notebook from '@/apis/notebooks'
+import { Message } from 'element-ui';
+console.log(Message)
 const state = {
   notebooks: []
 }
@@ -9,17 +11,17 @@ const getters = {
 
 const mutations = {
   setNotebooks(state, payload) {
-    state.notebooks = payload.notebooks
+    state.notebooks = payload.notebooks;
   },
   addNotebook(state, payload) {
-    state.notebooks.unshift(payload.notebook)
+    state.notebooks.unshift(payload.notebook);
   },
   updateNotebook(state, payload) {
     let notebook = state.notebooks.find(notebook => notebook.id === payload.notebookId) || {};
-    notebook.title = payload.title
+    notebook.title = payload.title;
   },
   deleteNotebook(state, payload) {
-    state.notebooks = state.notebooks.filter(notebook => notebook.id !== payload.notebookId)
+    state.notebooks = state.notebooks.filter(notebook => notebook.id !== payload.notebookId);
   }
 }
 
@@ -32,16 +34,19 @@ const actions = {
   addNotebook({ commit }, payload) {
     Notebook.addNotebook({title: payload.title}).then(res => {
       commit('addNotebook', { notebook: res.data })
-    }) 
+      Message.success(res.msg)
+    })  
   },
   updateNotebook({ commit }, payload) {
     Notebook.updateNotebook(payload.notebookId, { title: payload.title }).then(res => {
       commit('updateNotebook', { notebookId: payload.notebookId, title: payload.title })
+      Message.success(res.msg)
     })
   },
   deleteNotebook({ commit }, payload) {
     Notebook.deleteNotebook(payload.notebookId).then(res => {
       commit('deleteNotebook', { notebookId: payload.notebookId })
+      Message.success(res.msg)
     })
   }
 }
